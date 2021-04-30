f6ee51d9c68d2b5ef7d555a7f888ede8adef489f
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-30 17:46:34 +0800

    copy-intro Add basic component

---
+++ src/components/CopyIntro/CopyIntro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CopyIntro = (props) => {
+
+  const { info, text, cta} = props;
+
+  return (
+    <div>
+      Copy
+    </div>
+  );
+}
+
+export default CopyIntro;
---
+++ src/components/CopyIntro/CopyIntro.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CopyIntro from './CopyIntro';
+
+export default {
+  title: 'CopyIntro',
+};
+
+
+export const Default = () => {
+  return (
+    <>
+      <CopyIntro/>
+    </>
+  );
+}
