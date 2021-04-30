88d5023234f529637b1de7632d19171b772ba1f1
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-30 16:30:53 +0800

    info Add new component

---
+++ src/components/Info/Info.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Info from './Info';
+
+export default {
+  title: 'Info',
+};
+
+
+export const Default = () => {
+  return (
+    <>
+      <Info
+        title="Arkival"
+        role="Co-Founder & Creative Director"
+        type="UI/UX & Branding"
+        date="2021"
+       />
+    </>
+  );
+}
