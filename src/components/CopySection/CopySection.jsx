8c5a8bbff5fbffc02f017926b7a79433d1ae6d8b
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-03 16:32:41 +0800

    copy-section Pass content data through props

--- src/components/CopySection/CopySection.jsx
+++ src/components/CopySection/CopySection.jsx
@@ -4,14 +4,13 @@ import Section from '../Section/Section';
 import TextBlock from '../TextBlock/TextBlock';
 
 const Body = (props) => {
-
+  const { data } = props;
   return (
     <Section contain>
       <Grid columnGap='30px' columns="repeat(12, 1fr)">
         <Grid.Item column='4 / span 6'>
           <TextBlock
-            content="<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-            <p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"
+            content={data}
           />
         </Grid.Item>
       </Grid>
@@ -22,21 +21,19 @@ const Body = (props) => {
 export { Body };
 
 const CopySection = (props) => {
-
+  const { data } = props;
   return (
     <Section contain>
       <Grid columnGap='30px' columns="repeat(12, 1fr)">
         <Grid.Item column='1 / span 6'>
-          <TextBlock
-            content="<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-            <p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"
-          />
+          { data && data[0] ? <TextBlock
+            content={data[0]}
+          /> : '' }
         </Grid.Item>
         <Grid.Item column='7 / span 6'>
-          <TextBlock
-            content="<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
-            <p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"
-          />
+          { data && data[1] ? <TextBlock
+            content={data[1]}
+          /> : '' }
         </Grid.Item>
       </Grid>
     </Section>
--- src/components/CopySection/CopySection.stories.js
+++ src/components/CopySection/CopySection.stories.js
@@ -9,7 +9,8 @@ export default {
 export const Body = () => {
   return (
     <>
-      <CopyBody/>
+      <CopyBody data="<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
+            <p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"/>
     </>
   );
 }
@@ -17,7 +18,10 @@ export const Body = () => {
 export const Section = () => {
   return (
     <>
-      <CopySection/>
+      <CopySection data={[
+        "<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p><p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>",
+        "<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p><p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"
+      ]}/>
     </>
   );
 }
