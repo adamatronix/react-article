5d0b63839d4125cbce3b348baee268bcb7b6012b
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-05 15:45:49 +0800

    mock-page Add copy section with two columns

--- src/App.js
+++ src/App.js
@@ -46,6 +46,10 @@ function App() {
         width: 1368,
         height: 600
       }} alt="test" caption="1."/>
+      <CopySection data={[
+        "<p>Styleguide</p>",
+        "<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p><p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p><p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p><p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"
+      ]}/>
     </>
   );
 }
