9a7a17d20aa4ae5750ff5d655401229f2c2f65ac
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-05 14:44:35 +0800

    mock-page Add dev hynes hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import Hero from './components/Hero/Hero';
+import featuredImage from './assets/20201219_DevHynes_00136.jpeg';
+import featuredImageBlur from './assets/20201219_DevHynes_00136_blur.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <>
+      <Hero src={featuredImage} placeholder={featuredImageBlur} />
     </>
   );
 }
