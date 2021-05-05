8b9248d393d2be59378c68db8042c77bb2150ab9
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-05 16:01:29 +0800

    mock-page Add some copy to the bottom of the page

--- src/App.js
+++ src/App.js
@@ -68,6 +68,10 @@ function App() {
         width: 1240,
         height: 700
       }} alt="test"/>
+      <CopyBody data="
+<p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui.</p>
+<p>Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>"/>
     </>
   );
 }
