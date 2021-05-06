244b0b14204a5bba26cae1980b53826a1ecb29fe
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-06 18:05:52 +0800

    config-mobile Make copy section mobile

--- src/components/CopySection/CopySection.jsx
+++ src/components/CopySection/CopySection.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Grid from '@react-css/grid';
 import Section from '../Section/Section';
 import TextBlock from '../TextBlock/TextBlock';
 
 const Body = (props) => {
   const { data } = props;
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
   return (
     <Section contain>
       <Grid columnGap='30px' columns="repeat(12, 1fr)">
-        <Grid.Item column='4 / span 6'>
+        <Grid.Item column={isDesktop ? '4 / span 6' : '1 / span 12'}>
           <TextBlock
             content={data}
           />
@@ -22,15 +24,16 @@ export { Body };
 
 const CopySection = (props) => {
   const { data } = props;
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
   return (
     <Section contain>
       <Grid columnGap='30px' columns="repeat(12, 1fr)">
-        <Grid.Item column='1 / span 6'>
+        <Grid.Item column={isDesktop ? '1 / span 6' : '1 / span 12'}>
           { data && data[0] ? <TextBlock
             content={data[0]}
           /> : '' }
         </Grid.Item>
-        <Grid.Item column='7 / span 6'>
+        <Grid.Item column={ isDesktop ? '7 / span 6' : '1 / span 12'}>
           { data && data[1] ? <TextBlock
             content={data[1]}
           /> : '' }
