bf940a32f73852a6dc74b1f48d2c19f5ed016dca
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-07 10:48:27 +0800

    config-mobile Add a smaller gutter for mobile

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import cx from 'classnames/bind';
 import styles from './styles/section.module.scss';
 
 const Section = (props) => {
   const { children, small, full, contain, className} = props;
-
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
   const sectionClasses = cx(styles.default, className, {
     [styles.small]: small,
-    [styles['gutter-30']]: !full,
+    [styles['gutter-30']]: !full && isDesktop,
+    [styles['gutter-15']]: !full && !isDesktop,
     [styles['contain']]: contain
   })
 
