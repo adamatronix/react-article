5246a985bd52362a045f18fc4ce5186f17fcd9a7
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-07 10:40:10 +0800

    config-mobile Add a mobile section class to the copy section to create a bottom margin

--- src/components/CopySection/CopySection.jsx
+++ src/components/CopySection/CopySection.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import cx from 'classnames/bind';
 import Grid from '@react-css/grid';
 import Section from '../Section/Section';
 import TextBlock from '../TextBlock/TextBlock';
+import styles from './styles/copy-section.module.scss';
 
 const Body = (props) => {
   const { data } = props;
@@ -25,15 +27,20 @@ export { Body };
 const CopySection = (props) => {
   const { data } = props;
   const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
+
+  const itemClasses = cx({
+    [styles['section-item']]: !isDesktop
+  });
+
   return (
     <Section contain>
       <Grid columnGap='30px' columns="repeat(12, 1fr)">
-        <Grid.Item column={isDesktop ? '1 / span 6' : '1 / span 12'}>
+        <Grid.Item className={itemClasses} column={isDesktop ? '1 / span 6' : '1 / span 12'}>
           { data && data[0] ? <TextBlock
             content={data[0]}
           /> : '' }
         </Grid.Item>
-        <Grid.Item column={ isDesktop ? '7 / span 6' : '1 / span 12'}>
+        <Grid.Item className={itemClasses} column={ isDesktop ? '7 / span 6' : '1 / span 12'}>
           { data && data[1] ? <TextBlock
             content={data[1]}
           /> : '' }
