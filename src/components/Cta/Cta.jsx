9c91975a90f3b7b599510c0a84bc27e3c87f3a07
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-07 11:37:43 +0800

    config-mobile Handle cta mobile widths

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import cx from 'classnames/bind';
 import { ReactComponent as ArrowIcon} from './assets/Manual_MontrealArrow.svg';
 import styles from './styles/cta.module.scss';
 
 const CtaPrimary = (props) => {
   const { children } = props;
-
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
   return (
-    <button className={styles.button}>
+    <button className={cx(styles.button, {[styles['button--desktop']]: isDesktop})}>
       { children }
       <ArrowIcon className={styles['primary-arrow']}/>
     </button>
@@ -18,9 +19,9 @@ export { CtaPrimary };
 
 const CtaSecondary = (props) => {
   const { children } = props;
-
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
   return (
-    <button className={cx(styles.button, styles['button--secondary'])}>
+    <button className={cx(styles.button, styles['button--secondary'], {[styles['button--desktop']]: isDesktop})}>
       { children }
       <ArrowIcon className={styles['secondary-arrow']}/>
     </button>
