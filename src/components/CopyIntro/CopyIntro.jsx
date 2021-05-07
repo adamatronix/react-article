9e39a656cc631fdf6bd14e03de9ba8414b5585e6
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-07 12:28:13 +0800

    config-mobile Add Info mobile and CopyIntro modifications

--- src/components/CopyIntro/CopyIntro.jsx
+++ src/components/CopyIntro/CopyIntro.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@react-css/grid';
+import { useMediaQuery } from 'react-responsive';
 import Section from '../Section/Section';
 import Info from '../Info/Info';
 import TextBlock from '../TextBlock/TextBlock';
@@ -9,18 +10,19 @@ import styles from './styles/copy-intro.module.scss';
 const CopyIntro = (props) => {
 
   const { info, text, cta} = props;
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
 
   return (
     <Section className={styles.wrapper} contain>
       <Grid columnGap='30px' columns="repeat(12, 1fr)">
-        <Grid.Item column='1 / span 6'>
+        <Grid.Item column={ isDesktop ? '1 / span 6' : '1 / span 12'}>
           { info ? <Info 
             title={info.title}
             role={info.role}
             type={info.type}
             date={info.date}
           /> : ''}
-          { cta ? 
+          { cta && isDesktop ? 
             <a href={cta.href} target="_blank">
               <CtaPrimary>
                 { cta.label }
@@ -29,7 +31,7 @@ const CopyIntro = (props) => {
              : ''
           }
         </Grid.Item>
-        <Grid.Item column='7 / span 6'>
+        <Grid.Item column={ isDesktop ? '7 / span 6' : '1 / span 12'}>
           { text ? <TextBlock
             content={text}
           /> : ''}
--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import cx from 'classnames/bind';  
+import { useMediaQuery } from 'react-responsive';
 import styles from './styles/info.module.scss';
 
 const Info = (props) => {
 
   const { title, role, type, date} = props;
+  const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
 
   return (
-    <div className={styles.wrapper}>
+    <div className={cx(styles.wrapper, {[styles['wrapper--desktop']]: isDesktop})}>
       <h2 className={styles.title} >{ title }</h2>
       <div className={styles.role}>{ role }</div>
       <div className={styles.type}>{ type }</div>
